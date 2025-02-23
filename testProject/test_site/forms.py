99a2acd03b7b9c72fe32f5fa126c345ea20cff5e
Красавчик <stech999@ya.ru>
2025-02-23 17:30:22 +0300

    настроена корзина, поиск и другие мелочи

--- testProject/test_site/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from django.contrib.auth.forms import UserCreationForm
-
-class RegistrationForm(UserCreationForm):
-    class Meta(UserCreationForm.Meta):
-        model = UserCreationForm.Meta.model
-        fields = UserCreationForm.Meta.fields + ('email',)  # Добавьте дополнительные поля, если нужно
